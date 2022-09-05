a1a92ec31e91e980fd66da265e88e02bfa8c894c
Mollik Rahman <mollikster@gmail.com>
2022-09-04 19:03:32 -0500

    finished handleEvents.js

--- src/functions/handlers/handleEvents.js
+++ src/functions/handlers/handleEvents.js
@@ -7,6 +7,18 @@ module.exports = (client) => {
       const eventFiles = fs
         .readdirSync(`./src/event/${folder}`)
         .filter((file) => file.endsWith(".js"));
+      switch (folder) {
+        case "client":
+          for (const file of eventFiles) {
+            const event = require(`../../events${folder}/${file}`);
+            if (event.once) client.once(event.name, (...args) => event.execute(...args, client));
+            else client.on(event.name, (...args) => event.execute(...args, client));
+          }
+          break;
+          
+        default:
+          break;
+      }
     }
   };
 };
